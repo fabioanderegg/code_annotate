9ec7ad997717c50c9b8ed94926a4bd7777ab2bc6
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-05-31 16:45:21 +0200

    Redirect from root to browse page

--- code_annotate/urls.py
+++ code_annotate/urls.py
@@ -3,13 +3,14 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.conf import settings
 import django.views.static
-
+from django.views.generic.base import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('apps.web.urls', namespace='web')),
+    url(r'^$', RedirectView.as_view(url='/browse/?path=/')),
 ]
 
 # This is only needed when using runserver.
